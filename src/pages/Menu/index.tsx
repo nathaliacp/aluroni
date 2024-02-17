633a473408b28994ae5e3e84a636ada9a58c25c7
nathaliacp <nathaliapinheiro3@outlook.com>
2024-02-17 16:21:44 -0300

    add vite svgr lib and change img to a react component

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,11 +1,12 @@
 import style from "./Menu.module.scss";
+import Logo from "../../assets/logo.svg?react";
 
 export default function Menu() {
 
   return(
     <main>
-      <nav className={}>
-
+      <nav className={ style.menu }>
+        <Logo/>
       </nav>
     </main>
   );
